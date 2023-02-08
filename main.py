e027b866ea1a6cfb38321375fe88bb54c28915c0
mutish <mutindastacey@gmail.com>
2023-02-08 17:52:26 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+greeting = "Hello World"
+print(greeting)
+miles = 1000.0  #float variable
+# casting ;  specifying the data type variable
+x = str(3)  # string 3
+y = int(3)  # always be interpreted as an integer
+z = float(3) # 3.0 : float
+print(int(miles))
+# get the data type of an existing variable
+xy = '5'
+a = '5'
+c = int(xy) + int(a)
+# type(): returns the data type of the varable
+print(c)
+print(type(xy))
+# variables in python are Case sensitive
+name1 = "Stacey"
+Name1 = "Casey" #Name1 will not overwrite name1
+print(name1)
+
+
+fruit1, fruit2, fruit3 = 'orange', 'Banana', 'Cherry'
+print(fruit1)
+print(fruit2)
+print(fruit3)
+
+fruit4 = fruit5 = fruit6 = 'Apple'
+print(fruit4)
+print(fruit5)
+print(fruit6)
+
+fruits = ["apple", "cherry", "banana"]
+f1,f2,f3 = fruits
+print(f1)
+print(f2)
+print(f3)
---
+++ tasks.py
@@ -0,0 +1,104 @@
+# We use operators to manipulate the value of operands (Values/Variables)
+# Types of operators
+# 1. Arithmetic operators (+,-,/,*,%,**,//)
+# 2. Comparison operators (>,<,<=,>=,!=,==)
+# 3. Assignment Operators (=,+=,-=,*=,%=,**=,//=)
+# 4. Logical Operators (and,or,not)
+# # week 2
+# others , membership operators / bitwise / identity operators
+
+#COMPARISON OPERATORS
+#  ==  Equal  4==5  false. 4 == 4 true
+#!=  Not Equal 4!= 5 true
+#>  greater than 4 > 5 false. 5 > 4  true
+#<  less than 4 < 5  true
+#>= Greater than or equal to
+
+
+
+
+a = 4
+b = 5
+#equal
+print("a == b: ", a == b)
+
+#Not eqaul
+print("a != b", a != b)
+
+# Greater than
+print("a > b", a > b)
+
+#Less Than
+print("a < b", a < b)
+
+# Greater than or Equal to
+print("a >= b", a >= b)
+
+#Less than
+print("a <= b", a <= b)
+
+#Assignment Operators
+#  =   Assignment operator  a = 10
+#  +=  Addition Assignment  a += 5 (same as a
+#  Subtraction Assignment
+# Multiplication Assignment
+# Division Assignment
+#  Remainder Assignment
+# Exponentation Assignment
+# Floor Division Assignment
+
+
+a = 10
+
+print("a += 5: ", a)
+
+print("a -= 5: ", a)
+
+print("a *= 5: ", a)
+
+print("a /= 5: ", a)
+
+print("a %= 5: ", a)
+
+print("a **= 5: ", a)
+
+print("a //= 5: ", a)
+
+
+# Logical operators
+# and Logica AND   if both operators are true then condition becomes true AND&&
+# or Logical OR   if any of the two operands are non- zero the condition becomes true OR ||
+# not Logical NOT   used to reverse the logical state of its operand !
+
+# Conditionals or Control flow statements are used together with logical operators
+# If ... ELSE ... statement
+# if condition:
+#      # code to execute if condition is true
+# else:
+#     # code to execute if condition is false
+
+variableCompare = 5
+secondVariable = 10
+
+if variableCompare == 10:
+    print("simple if")
+else:
+    print("condition not met")
+
+if variableCompare == 10:
+    print("from multi-conditional")
+elif variableCompare == 7:
+    print("from the multi-conditional")
+elif variableCompare == 7:
+    print("from the multi-conditional")
+else:
+    print("none meet condition")
+
+
+if variableCompare == 10 or secondVariable == 6:
+    print("from or")
+elif variableCompare == 5 and secondVariable == 10:
+    print("from and")
+else:
+    print("No condition met")
+
